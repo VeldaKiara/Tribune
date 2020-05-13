33ec29cfd6727ac08e175a44c976a46235da7954
Velda Kiara <veldakarimi@gmail.com>
2020-05-13 15:30:46 +0300

    addition of urls

--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 # Create your views here.
+#A view is a function that takes the request from a user,
+#  processes it and returns a response to the user
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('Welcome to the Daily Tribune')
