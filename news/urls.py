b1ec74a38b795f259d68638a113bfc54819f5f32
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 05:56:27 +0300

    changes to news url

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.news_of_day,name='newsToday'),
+    url('',views.welcome,name = 'welcome'),
+    url('',views.news_of_day,name='newsToday'),
 ]
