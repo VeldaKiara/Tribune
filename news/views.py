5107b639271cf5271c9cc6cb2f48175e86ac4b31
Velda Kiara <veldakarimi@gmail.com>
2020-05-15 05:50:07 +0300

    addition to chaanges on dates and how to get news for specific days

--- news/views.py
+++ news/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-# Create your views here.
+import datetime as dt
 #A view is a function that takes the request from a user,
 #  processes it and returns a response to the user
 def welcome(request):
-    return HttpResponse('Welcome to the Daily Tribune')
+    return HttpResponse('Welcome to the Daily Tribune')  
+
+#function to get news for a particular day.  
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)   
+       
+#date weekdday function
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day   
+
+#passing request to get exact day         
+def news_of_day(request):
+    date = dt.date.today()
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)          
+
+        
